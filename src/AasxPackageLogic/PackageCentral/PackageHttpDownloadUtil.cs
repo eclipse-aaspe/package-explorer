1a8265070dfab89c4d1f96b7d7a5f17cd415f8c3
Michael Hoffmeister <michael.hoffmeister@festo.com>
2024-10-12 15:50:07 +0200

    * messing up to much async

--- src/AasxPackageLogic/PackageCentral/PackageHttpDownloadUtil.cs
+++ src/AasxPackageLogic/PackageCentral/PackageHttpDownloadUtil.cs
@@ -81,7 +81,8 @@ namespace AasxPackageLogic.PackageCentral
             return res;
         }
 
-        public static async Task HttpGetToMemoryStream(
+        // TODO: Refactor
+        public static async Task HttpGetToMemoryStreamOLD(
             Uri sourceUri,
             Action<MemoryStream, string> lambdaDownloadDone,
             PackCntRuntimeOptions runtimeOptions = null,
@@ -286,6 +287,218 @@ namespace AasxPackageLogic.PackageCentral
             }
         }
 
+        /// <summary>
+        /// Try get an HTTP ressource by GET.
+        /// </summary>
+        /// <param name="lambdaDownloadDoneOrFail">Called also for failed status codes!</param>
+        /// <exception cref="Exception">Any exception might occur outside the HTTP status codes.</exception>
+        public static async Task HttpGetToMemoryStream(
+            Uri sourceUri,
+            Action<HttpStatusCode, MemoryStream, string> lambdaDownloadDoneOrFail,
+            PackCntRuntimeOptions runtimeOptions = null,
+            PackageContainerListBase containerList = null,
+            bool allowFakeResponses = false)
+        {
+            // access
+            if (sourceUri == null)
+                return;
+
+            // check for fake answers?
+            if (allowFakeResponses && _fakeAnswers?.Http != null)
+                foreach (var fa in _fakeAnswers.Http)
+                    if (fa.Request.Equals(sourceUri.ToString(), StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        using (var memStream = new MemoryStream())
+                        using (var writer = new StreamWriter(memStream))
+                        {
+                            writer.Write(AdminShellUtil.Base64Decode(fa.Response));
+                            writer.Flush();
+                            memStream.Flush();
+                            memStream.Seek(0, SeekOrigin.Begin);
+                            lambdaDownloadDoneOrFail?.Invoke(HttpStatusCode.OK, memStream, "content.json");
+                            return;
+                        }
+                    }
+
+            // 
+
+            // read via HttpClient (uses standard proxies)
+            var handler = new HttpClientHandler();
+            handler.DefaultProxyCredentials = CredentialCache.DefaultCredentials;
+            handler.AllowAutoRedirect = false;
+
+            var client = new HttpClient(handler);
+
+            client.DefaultRequestHeaders.Add("Accept", "application/aas");
+            client.BaseAddress = new Uri(sourceUri.GetLeftPart(UriPartial.Authority));
+            var requestPath = sourceUri.PathAndQuery;
+
+            // Log
+            runtimeOptions?.Log?.Info($"HttpClient GET() with base-address {client.BaseAddress} " +
+                $"and request {requestPath} .. ");
+
+            // Token existing?
+            var clhttp = containerList as PackageContainerListHttpRestBase;
+            var oidc = clhttp?.OpenIdClient;
+            if (oidc == null)
+            {
+                runtimeOptions?.Log?.Info("  no ContainerList available. No OpecIdClient possible!");
+                if (clhttp != null && OpenIDClient.email != "")
+                {
+                    clhttp.OpenIdClient = new OpenIdClientInstance();
+                    clhttp.OpenIdClient.email = OpenIDClient.email;
+                    clhttp.OpenIdClient.ssiURL = OpenIDClient.ssiURL;
+                    clhttp.OpenIdClient.keycloak = OpenIDClient.keycloak;
+                    oidc = clhttp.OpenIdClient;
+                }
+            }
+            if (oidc != null)
+            {
+                if (oidc.token != "")
+                {
+                    runtimeOptions?.Log?.Info($"  using existing bearer token.");
+                    client.SetBearerToken(oidc.token);
+                }
+                else
+                {
+                    if (oidc.email != "")
+                    {
+                        runtimeOptions?.Log?.Info($"  using existing email token.");
+                        client.DefaultRequestHeaders.Add("Email", OpenIDClient.email);
+                    }
+                }
+            }
+
+            bool repeat = true;
+
+            while (repeat)
+            {
+                // get response?
+                var response = await client.GetAsync(requestPath,
+                    HttpCompletionOption.ResponseHeadersRead);
+
+                if (clhttp != null
+                    && response.StatusCode == System.Net.HttpStatusCode.TemporaryRedirect)
+                {
+                    string redirectUrl = response.Headers.Location.ToString();
+                    // ReSharper disable once RedundantExplicitArrayCreation
+                    string[] splitResult = redirectUrl.Split(new string[] { "?" },
+                        StringSplitOptions.RemoveEmptyEntries);
+                    splitResult[0] = splitResult[0].TrimEnd('/');
+
+                    if (splitResult.Length < 1)
+                    {
+                        runtimeOptions?.Log?.Error("TemporaryRedirect, but url split to successful");
+                        break;
+                    }
+
+                    runtimeOptions?.Log?.Info("Redirect to: " + splitResult[0]);
+
+                    if (oidc == null)
+                    {
+                        runtimeOptions?.Log?.Info("Creating new OpenIdClient..");
+                        oidc = new OpenIdClientInstance();
+                        clhttp.OpenIdClient = oidc;
+                        clhttp.OpenIdClient.email = OpenIDClient.email;
+                        clhttp.OpenIdClient.ssiURL = OpenIDClient.ssiURL;
+                        clhttp.OpenIdClient.keycloak = OpenIDClient.keycloak;
+                    }
+
+                    oidc.authServer = splitResult[0];
+
+                    runtimeOptions?.Log?.Info($".. authentication at auth server {oidc.authServer} needed");
+
+                    var response2 = await oidc.RequestTokenAsync(null,
+                        GenerateUiLambdaSet(runtimeOptions));
+                    if (oidc.keycloak == "" && response2 != null)
+                        oidc.token = response2.AccessToken;
+                    if (oidc.token != "" && oidc.token != null)
+                        client.SetBearerToken(oidc.token);
+
+                    repeat = true;
+                    continue;
+                }
+
+                repeat = false;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var contentLength = response.Content.Headers.ContentLength;
+                    var contentFn = response.Content.Headers.ContentDisposition?.FileName;
+
+                    // log
+                    runtimeOptions?.Log?.Info($".. response with header-content-len {contentLength} " +
+                        $"and file-name {contentFn} ..");
+
+                    var contentStream = await response?.Content?.ReadAsStreamAsync();
+                    if (contentStream == null)
+                        throw new PackageContainerException(
+                        $"While getting data bytes from {sourceUri.ToString()} via HttpClient " +
+                        $"no data-content was responded!");
+
+                    // create temp file and write to it
+                    var givenFn = sourceUri.ToString();
+                    if (contentFn != null)
+                        givenFn = contentFn;
+                    runtimeOptions?.Log?.Info($".. downloading and scanning by proxy/firewall {client.BaseAddress} " +
+                        $"and request {requestPath} .. ");
+
+                    using (var memStream = new MemoryStream())
+                    {
+                        // copy with progress
+                        var bufferSize = 4024;
+                        var deltaSize = 512 * 1024;
+                        var buffer = new byte[bufferSize];
+                        long totalBytesRead = 0;
+                        long lastBytesRead = 0;
+                        int bytesRead;
+
+                        runtimeOptions?.ProgressChanged?.Invoke(PackCntRuntimeOptions.Progress.Starting,
+                                contentLength, totalBytesRead);
+
+                        while ((bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length,
+                            default(CancellationToken)).ConfigureAwait(false)) != 0)
+                        {
+                            await memStream.WriteAsync(buffer, 0, bytesRead,
+                                default(CancellationToken)).ConfigureAwait(false);
+
+                            totalBytesRead += bytesRead;
+
+                            if (totalBytesRead > lastBytesRead + deltaSize)
+                            {
+                                runtimeOptions?.Log?.Info($".. downloading to memory stream");
+                                runtimeOptions?.ProgressChanged?.Invoke(PackCntRuntimeOptions.Progress.Ongoing,
+                                    contentLength, totalBytesRead);
+                                lastBytesRead = totalBytesRead;
+                            }
+                        }
+
+                        // assume bytes read to be total bytes
+                        runtimeOptions?.ProgressChanged?.Invoke(PackCntRuntimeOptions.Progress.Final,
+                            totalBytesRead, totalBytesRead);
+
+                        // log                
+                        runtimeOptions?.Log?.Info($".. download done with {totalBytesRead} bytes read!");
+
+                        // execute lambda
+                        memStream.Flush();
+                        memStream.Seek(0, SeekOrigin.Begin);
+                        lambdaDownloadDoneOrFail?.Invoke(response.StatusCode, memStream, contentFn);
+                    }
+                }
+                else
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    lambdaDownloadDoneOrFail?.Invoke(response.StatusCode, null, null);
+                }
+                else
+                {
+                    Log.Singleton.Error($"DownloadFromSource server gave status code {response.StatusCode}!");
+                    lambdaDownloadDoneOrFail?.Invoke(response.StatusCode, null, null);
+                }
+            }
+        }
+
         public static async Task<HttpStatusCode?> HttpPostRequestToMemoryStream(
             Uri sourceUri,
             string requestContentType,
@@ -347,7 +560,7 @@ namespace AasxPackageLogic.PackageCentral
             }
 
             // prepare request
-            var requestContent = new StringContent(requestBody,   Encoding.UTF8, requestContentType);
+            var requestContent = new StringContent(requestBody, Encoding.UTF8, requestContentType);
 
             // retrieve response
             bool repeat = true;
