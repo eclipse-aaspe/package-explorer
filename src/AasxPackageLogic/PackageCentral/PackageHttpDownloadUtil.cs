f331451b7f7c957dd16c051ba45239fc2197250f
Michael Hoffmeister <michael.hoffmeister@festo.com>
2024-10-28 17:47:32 +0100

    * update

--- src/AasxPackageLogic/PackageCentral/PackageHttpDownloadUtil.cs
+++ src/AasxPackageLogic/PackageCentral/PackageHttpDownloadUtil.cs
@@ -30,6 +30,8 @@ using System.Net.Http.Headers;
 using System.Text;
 using AasCore.Aas3_0;
 using System.ServiceModel;
+using Namotion.Reflection;
+using System.Text.Json.Nodes;
 
 namespace AasxPackageLogic.PackageCentral
 {
@@ -850,6 +852,103 @@ namespace AasxPackageLogic.PackageCentral
             }
         }
 
+        public static async Task<Tuple<HttpStatusCode, string>> HttpDeleteUri(
+            Uri delUri,
+            PackCntRuntimeOptions runtimeOptions = null,
+            PackageContainerListBase containerList = null)
+        {
+            // access
+            if (delUri == null)
+                return null;
+
+            // read via HttpClient (uses standard proxies)
+            var handler = new HttpClientHandler();
+            handler.DefaultProxyCredentials = CredentialCache.DefaultCredentials;
+
+            // new http client
+            var client = new HttpClient(handler);
+
+            client.BaseAddress = new Uri(delUri.GetLeftPart(UriPartial.Authority));
+            var requestPath = delUri.PathAndQuery;
+
+            // Log
+            if (runtimeOptions?.ExtendedConnectionDebug == true)
+                runtimeOptions.Log?.Info($"HttpClient DELETE() with base-address {client.BaseAddress} " +
+                    $"and request {requestPath} .. ");
+
+            // Token existing?
+            var clhttp = containerList as PackageContainerListHttpRestBase;
+            var oidc = clhttp?.OpenIdClient;
+            if (oidc == null)
+            {
+                if (runtimeOptions?.ExtendedConnectionDebug == true)
+                    runtimeOptions.Log?.Info("  no ContainerList available. No OpecIdClient possible!");
+                if (clhttp != null && OpenIDClient.email != "")
+                {
+                    clhttp.OpenIdClient = new OpenIdClientInstance();
+                    clhttp.OpenIdClient.email = OpenIDClient.email;
+                    clhttp.OpenIdClient.ssiURL = OpenIDClient.ssiURL;
+                    clhttp.OpenIdClient.keycloak = OpenIDClient.keycloak;
+                    oidc = clhttp.OpenIdClient;
+                }
+            }
+            if (oidc != null)
+            {
+                if (oidc.token != "")
+                {
+                    if (runtimeOptions?.ExtendedConnectionDebug == true)
+                        runtimeOptions.Log?.Info($"  using existing bearer token.");
+                    client.SetBearerToken(oidc.token);
+                }
+                else
+                {
+                    if (oidc.email != "")
+                    {
+                        if (runtimeOptions?.ExtendedConnectionDebug == true)
+                            runtimeOptions.Log?.Info($"  using existing email token.");
+                        client.DefaultRequestHeaders.Add("Email", OpenIDClient.email);
+                    }
+                }
+            }
+
+            // BEGIN Workaround behind some proxies
+            // Stream is sent twice, if proxy-authorization header is not set
+            string proxyFile = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + "/proxy.dat";
+            string username = "";
+            string password = "";
+            if (System.IO.File.Exists(proxyFile))
+            {
+                using (StreamReader sr = new StreamReader(proxyFile))
+                {
+                    // ReSharper disable MethodHasAsyncOverload
+                    sr.ReadLine();
+                    username = sr.ReadLine();
+                    password = sr.ReadLine();
+                    // ReSharper enable MethodHasAsyncOverload
+                }
+            }
+            if (username != "" && password != "")
+            {
+                var authToken = Encoding.ASCII.GetBytes(username + ":" + password);
+                client.DefaultRequestHeaders.ProxyAuthorization = new AuthenticationHeaderValue("Basic",
+                    Convert.ToBase64String(authToken));
+            }
+            // END Workaround behind some proxies
+
+            // get response?
+            using (var response = await client.DeleteAsync(requestPath))
+            {
+                // try read in any case, despite:
+                // https://stackoverflow.com/questions/17640666/httpclient-deleteasync-and-content-readadstringasync-always-return-null
+                var content = "";
+                if (response.StatusCode != HttpStatusCode.NoContent)
+                    await response.Content.ReadAsStringAsync();
+
+                // ok!
+                return new Tuple<HttpStatusCode, string>(response.StatusCode, content);
+            }
+        }
+
         public static async Task<Tuple<HttpStatusCode, string>> HttpPutPostIdentifiable(
             Aas.IIdentifiable idf,
             Uri destUri,
@@ -890,5 +989,240 @@ namespace AasxPackageLogic.PackageCentral
             }
         }
 
+        /// <summary>
+        /// This utility is able to parallel download Identifiables and will call lambda upon.
+        /// Insted of a list of location, it is taking a list of objects (entities) and a lambda
+        /// to extract the location from.
+        /// </summary>
+        /// <typeparam name="T">Type of Identifiable</typeparam>
+        /// <typeparam name="E">Type of entity element</typeparam>
+        public static async Task<int> DownloadListOfIdentifiables<T, E>(
+            IEnumerable<E> entities,
+            Func<E, Uri> lambdaGetLocation,
+            Action<HttpStatusCode, T, string, E> lambdaDownloadDoneOrFail,
+            PackCntRuntimeOptions runtimeOptions = null,
+            bool allowFakeResponses = false,
+            bool useParallel = false,
+            Func<E, Type> lambdaGetTypeToSerialize = null) where T : Aas.IIdentifiable
+        {
+            // access
+            if (entities == null)
+                return 0;
+
+            // result
+            int numRes = 0;
+
+            // lambda for deserialize
+            Func<JsonNode, E, T> lambdaDeserialize = (node, ent) =>
+            {
+                if (typeof(T).IsInterface && lambdaGetTypeToSerialize != null)
+                {
+                    var t = lambdaGetTypeToSerialize(ent);
+                    if (t.IsAssignableTo(typeof(Aas.IAssetAdministrationShell)))
+                        return (T)((Aas.IIdentifiable)Jsonization.Deserialize.AssetAdministrationShellFrom(node));
+                    if (t.IsAssignableTo(typeof(Aas.ISubmodel)))
+                        return (T)((Aas.IIdentifiable)Jsonization.Deserialize.SubmodelFrom(node));
+                    if (t.IsAssignableTo(typeof(Aas.IConceptDescription)))
+                        return (T)((Aas.IIdentifiable)Jsonization.Deserialize.ConceptDescriptionFrom(node));
+                }
+
+                if (typeof(T).IsAssignableFrom(typeof(Aas.IAssetAdministrationShell)))
+                    return (T)((Aas.IIdentifiable)Jsonization.Deserialize.AssetAdministrationShellFrom(node));
+                if (typeof(T).IsAssignableFrom(typeof(Aas.ISubmodel)))
+                    return (T)((Aas.IIdentifiable)Jsonization.Deserialize.SubmodelFrom(node));
+                if (typeof(T).IsAssignableFrom(typeof(Aas.IConceptDescription)))
+                    return (T)((Aas.IIdentifiable)Jsonization.Deserialize.ConceptDescriptionFrom(node));
+                return default(T);
+            };
+
+            // over all locations
+            if (!useParallel)
+            {
+                foreach (var ent in entities)
+                {
+                    await PackageHttpDownloadUtil.HttpGetToMemoryStream(
+                        sourceUri: lambdaGetLocation?.Invoke(ent),
+                        allowFakeResponses: allowFakeResponses,
+                        runtimeOptions: runtimeOptions,
+                        lambdaDownloadDoneOrFail: (code, ms, contentFn) =>
+                        {
+                            // not OK?
+                            if (code != HttpStatusCode.OK)
+                            {
+                                lambdaDownloadDoneOrFail?.Invoke(code, default(T), null, ent);
+                                return;
+                            }
+
+                            // go on
+                            try
+                            {
+                                var node = System.Text.Json.Nodes.JsonNode.Parse(ms);
+                                T idf = lambdaDeserialize(node, ent);
+                                lambdaDownloadDoneOrFail?.Invoke(code, idf, contentFn, ent);
+                                if (code == HttpStatusCode.OK)
+                                    numRes++;
+                            }
+                            catch (Exception ex)
+                            {
+                                runtimeOptions?.Log?.Error(ex, $"Parsing downloaded {typeof(T).GetDisplayName()}");
+                                lambdaDownloadDoneOrFail?.Invoke(HttpStatusCode.UnprocessableEntity, default(T), null, ent);
+                            }
+                        });
+                }
+            }
+            else
+            {
+                await Parallel.ForEachAsync(entities,
+                    new ParallelOptions() { MaxDegreeOfParallelism = Options.Curr.MaxParallelOps },
+                    async (ent, token) =>
+                    {
+                        var thisEnt = ent;
+                        await PackageHttpDownloadUtil.HttpGetToMemoryStream(
+                            sourceUri: lambdaGetLocation?.Invoke(ent),
+                            allowFakeResponses: allowFakeResponses,
+                            runtimeOptions: runtimeOptions,
+                            lambdaDownloadDoneOrFail: (code, ms, contentFn) =>
+                            {
+                                // not OK?
+                                if (code != HttpStatusCode.OK)
+                                {
+                                    lambdaDownloadDoneOrFail?.Invoke(code, default(T), null, ent);
+                                    return;
+                                }
+
+                                // go on
+                                try
+                                {
+                                    var node = System.Text.Json.Nodes.JsonNode.Parse(ms);
+                                    T idf = lambdaDeserialize(node, ent);
+                                    lambdaDownloadDoneOrFail?.Invoke(code, idf, contentFn, thisEnt);
+                                    if (code == HttpStatusCode.OK)
+                                        numRes++;
+                                }
+                                catch (Exception ex)
+                                {
+                                    runtimeOptions?.Log?.Error(ex, $"Parsing downloaded {typeof(T).GetDisplayName()}");
+                                    lambdaDownloadDoneOrFail?.Invoke(HttpStatusCode.UnprocessableEntity, default(T), null, thisEnt);
+                                }
+                            });
+                    });
+
+            }
+
+            // ok
+            return numRes;
+        }
+
+        public static async Task<T> DownloadIdentifiableToOK<T>(
+            Uri location,
+            PackCntRuntimeOptions runtimeOptions = null,
+            bool allowFakeResponses = false) where T : Aas.IIdentifiable
+        {
+            T res = default(T);
+
+            await DownloadListOfIdentifiables<T, Uri>(
+                new[] { location },
+                lambdaGetLocation: (loc) => loc,
+                runtimeOptions: runtimeOptions,
+                allowFakeResponses: allowFakeResponses,
+                lambdaDownloadDoneOrFail: (code, idf, contentFn, ent) =>
+                {
+                    if (code == HttpStatusCode.OK)
+                        res = idf;
+                });
+
+            return res;
+        }
+
+        /// <summary>
+        /// Can download arbitrary dynamic entity.
+        /// </summary>
+        /// <returns>Either dynamic object or <c>null</c></returns>
+        public static async Task<dynamic> DownloadEntityToDynamicObject(
+            Uri uri,
+            PackCntRuntimeOptions runtimeOptions = null,
+            bool allowFakeResponses = false)
+        {
+            // prepare receing the descriptors
+            dynamic resObj = null;
+
+            // GET
+            await HttpGetToMemoryStream(
+                sourceUri: uri,
+                allowFakeResponses: allowFakeResponses,
+                runtimeOptions: runtimeOptions,
+                lambdaDownloadDoneOrFail: (code, ms, contentFn) =>
+                {
+                    if (code != HttpStatusCode.OK)
+                        return;
+
+                    try
+                    {
+                        // try working with dynamic objects
+                        using (StreamReader reader = new StreamReader(ms, System.Text.Encoding.UTF8, true))
+                        using (var jsonTextReader = new JsonTextReader(reader))
+                        {
+                            JsonSerializer serializer = new JsonSerializer();
+                            resObj = serializer.Deserialize(jsonTextReader);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        runtimeOptions?.Log?.Error(ex, "Parsing initially downloaded AAS");
+                    }
+                });
+
+            return resObj;
+        }
+
+        public static async Task<int> DeleteListOfEntities<E>(
+            IEnumerable<E> entities,
+            Func<E, Uri> lambdaGetLocation,
+            Action<HttpStatusCode, string, E> lambdaDeleteDoneOrFail,
+            PackCntRuntimeOptions runtimeOptions = null,
+            bool allowFakeResponses = false,
+            bool useParallel = false) 
+        {
+            // access
+            if (entities == null)
+                return 0;
+
+            // result
+            int numRes = 0;
+
+            // over all locations
+            if (!useParallel)
+            {
+                foreach (var ent in entities)
+                {
+                    // delete
+                    var res = await HttpDeleteUri(
+                        delUri: lambdaGetLocation?.Invoke(ent),
+                        runtimeOptions: runtimeOptions);
+
+                    // tell
+                    lambdaDeleteDoneOrFail?.Invoke(res.Item1, res.Item2, ent);
+                }
+            }
+            else
+            {
+                await Parallel.ForEachAsync(entities,
+                    new ParallelOptions() { MaxDegreeOfParallelism = Options.Curr.MaxParallelOps },
+                    async (ent, token) =>
+                    {
+                        // delete
+                        var res = await HttpDeleteUri(
+                            delUri: lambdaGetLocation?.Invoke(ent),
+                            runtimeOptions: runtimeOptions);
+
+                        // tell
+                        lambdaDeleteDoneOrFail?.Invoke(res.Item1, res.Item2, ent);
+                    });
+
+            }
+
+            // ok
+            return numRes;
+        }
     }
 }
