8141e808c4a90c0e76e52ffc76b767a6c14280ca
PratikTarpara <pratik221297@gmail.com>
2025-06-06 00:32:16 +0200

    Changes

--- src/AasxPluginAssetInterfaceDesc/AidBacnetConnection.cs
+++ src/AasxPluginAssetInterfaceDesc/AidBacnetConnection.cs
@@ -4,10 +4,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AasxPredefinedConcepts;
+using Aas = AasCore.Aas3_0;
+using AdminShellNS;
+using Extensions;
+using AasxIntegrationBase;
+using AasxPredefinedConcepts.AssetInterfacesDescription;
+using FluentModbus;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Net.Http;
+using AdminShellNS.DiaryData;
+using AasxIntegrationBase.AdminShellEvents;
+using System.Drawing;
+using MQTTnet;
+using MQTTnet.Client;
+using System.Web.Services.Description;
+using AasxOpcUa2Client;
+using System.IO.BACnet;
+using System.Threading;
+using System.Diagnostics;
 
 namespace AasxPluginAssetInterfaceDescription
 {
     public class AidBacnetConnection : AidBaseConnection
     {
-    }
+
+    }    
 }
+       
